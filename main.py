868d39353eacb1f0b6ad4fce2ad75751fefa87af
Mihail Kolev <kolev210@gmail.com>
2024-12-08 20:51:02 +0200

    created count words function

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world")
+def count_number_of_words(text):
+    number_of_words = 0
+    
+    return number_of_words
+
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        print(file_contents)
+if __name__ == "__main__":
+    main()
