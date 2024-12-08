cf50e17cd988c8d1a221041fe0a7c2cd4fcd6b2e
Mihail Kolev <kolev210@gmail.com>
2024-12-08 21:32:11 +0200

    count_number_of_words function fully functioning

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-def count_number_of_words(text):
-    number_of_words = 0
-    
-    return number_of_words
+def count_number_of_words(mystring):
+    try:
+        words_list = mystring.split()
+        count = 0
+       
+        for word in words_list:
+            count += 1
+        return count
+    except Exception:
+         error = "Not a string"
+         return error
+
 
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        test_count_words = "Project Gutenberg-tm eBooks are often"
+        print(count_number_of_words(file_contents))
 if __name__ == "__main__":
     main()
